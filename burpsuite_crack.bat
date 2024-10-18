a600242ad7e385173e1afd0af95ca74b0bdac0a0
A <38929533+20142995@users.noreply.github.com>
2024-10-18 14:44:11 +0800

    Add files via upload

---
+++ burpsuite_crack.bat
@@ -0,0 +1,4 @@
+@pushd %~dp0
+@@call oraclejdk
+@start javaw -jar burpsuitloader.jar
+@popd
---
+++ burpsuite_loader.bat
@@ -0,0 +1,4 @@
+@pushd %~dp0
+@@call oraclejdk
+@start javaw -XX:+IgnoreUnrecognizedVMOptions -javaagent:burpsuitloader.jar=loader, --add-opens=java.desktop/javax.swing=ALL-UNNAMED --add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.tree=ALL-UNNAMED --add-opens=java.base/jdk.internal.org.objectweb.asm.Opcodes=ALL-UNNAMED -jar burpsuite_pro.jar
+@popd
